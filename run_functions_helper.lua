1844ba84346bc3f47841f86154848ac63ae8ef7f
luqdude <215190855+luq-dude@users.noreply.github.com>
2025-12-17 13:31:17 -0500

    chore: remove unused function

--- run_functions_helper.lua
+++ run_functions_helper.lua
@@ -46,59 +46,6 @@ function RunHelper:reorder_card_area(card_area, new_indicies)
     card_area:align_cards()
 end
 
-function RunHelper:get_query_string(state)
-    state = state or G.STATE
-    local query_string = ""
-    local state_string = ""
-    if state == G.STATES.SELECTING_HAND or state == G.STATES.PLAY_TAROT then
-        query_string = "It's time to pick cards in your hand to play or discard. You can also use consumables re-order jokers, or sell either jokers or consumables."
-        state_string = "You have " .. tostring(G.GAME.current_round.hands_left) .. " hands left and " .. tostring(G.GAME.current_round.discards_left) .. " discards left. " ..
-                        "You have " .. tostring(#G.deck.cards) .. " cards remaining in your deck that can be drawn, out of the " .. tostring(G.deck.config.card_limit) .. " cards in it.\n"
-
-        if #state_string ~= 0 then
-            state_string = state_string .. "\n"
-        end
-        if G.GAME.blind.boss then
-            state_string = state_string .. "These are the cards in your hand, their modifiers and if they are debuffed. Debuffed cards do not get scored: "
-        else
-            state_string = state_string .. "These are the cards in your hand and their modifiers: "
-        end
-        state_string = state_string .. table.table_to_string(GetRunText.get_hand_details(G.hand.cards, true))
-    elseif state == G.STATES.SHOP then
-        query_string = "You are now in the shop! You can use your money to buy cards, booster packs or vouchers to help your run. You can also use consumables and sell jokers/consumables you no longer need. When done shopping, you can exit the shop to blind selection."
-        state_string = "You currently have $" .. tostring(G.GAME.dollars) .. " to spend."
-    elseif state == 999 then
-        if SMODS.OPENED_BOOSTER.config.center.draw_hand then
-            query_string = "You have opened a booster pack containing consumables and can now pick a consumable to immediately use from it. You can also select cards from your hand to use if the consumable needs it."
-        else
-            query_string = "You have opened a booster pack containing cards and can now select cards to keep from it."
-        end
-        state_string = RunContext.booster()
-    end
-
-    state_string = state_string .. string.format("\nYou currently have %d/%d jokers in your inventory.", #G.jokers.cards, G.jokers.config.card_limit)
-    if #G.jokers.cards > 0 then
-        local cards = GetRunText.get_hand_details(G.jokers.cards, false, false, nil, true)
-
-        for pos, value in ipairs(cards) do
-            cards[pos] = "\n" .. pos .. ": " .. value .. ". Sell value: $" .. G.jokers.cards[pos].sell_cost
-        end
-        state_string = state_string .. " Here is a list of their abilities, modifiers and sell value: ".. table.concat(cards, "", 1, #cards)
-    end
-
-    state_string = state_string .. string.format("\nYou currently have %d/%d consumables in your inventory.", #G.consumeables.cards, G.consumeables.config.card_limit)
-    if #G.consumeables.cards > 0 then
-        local cards = GetRunText.get_hand_details(G.consumeables.cards)
-
-        for pos, value in ipairs(cards) do
-            cards[pos] = "\n" .. pos .. ": " .. value .. ". Sell value: $" .. G.consumeables.cards[pos].sell_cost
-        end
-        state_string = state_string .. " Here is a list of their abilities and sell value: " .. table.concat(cards, "", 1, #cards)
-    end
-
-    return query_string, state_string
-end
-
 function RunHelper:get_consumable_validation(card,selected_hand_index,selected_action,forced_selection)
     selected_action = selected_action or "Use"
     forced_selection = forced_selection or false
