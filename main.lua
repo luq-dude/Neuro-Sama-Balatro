9def98937aadf222ab633a3df76b8af41087fc44
luqdude <215190855+luq-dude@users.noreply.github.com>
2025-12-18 00:45:48 -0500

    refactor: renamed card_info.lua -> consumable_validation.lua

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ assert(SMODS.load_file("game-sdk/utils/table_utils.lua"))()
 assert(SMODS.load_file("game-sdk/sdk_string_consts.lua"))()
 
 assert(SMODS.load_file("modifier_loc.lua"))()
-assert(SMODS.load_file("card_info.lua"))()
+assert(SMODS.load_file("consumable_validation.lua"))()
 assert(SMODS.load_file("deck_loc.lua"))()
 
 NEURO.MOD_CACHE = assert(SMODS.load_file("module_cache.lua"))()
