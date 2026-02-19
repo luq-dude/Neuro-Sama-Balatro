318cff8c4fd7ad8c145d9e3bae9af3a1212d5dad
luqdude <215190855+luq-dude@users.noreply.github.com>
2026-02-18 22:08:19 -0500

    fix: fix crash for real this time

--- modes/coop.lua
+++ modes/coop.lua
@@ -255,7 +255,7 @@ function CoopMode:in_shop()
             delay = 2 * G.SPEEDFACTOR,
             blocking = false,
             func = function()
-                if NEURO.STATE ~= NEURO.STATES.IN_SHOP or G.STATE ~= G.STATES.IN_SHOP then
+                if NEURO.STATE ~= NEURO.STATES.IN_SHOP or G.STATE ~= G.STATES.SHOP then
                     return true
                 end
                 local ctx = RunContext.get_shop_context()
